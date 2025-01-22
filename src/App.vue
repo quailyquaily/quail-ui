677b6471b9b7a5db2b3a74ea785d7cdef8b13fef
Lyric <5h3ll3x@gmail.com>
2025-01-22 19:53:26 +0900

    Add SecForm component and integrate into App.vue; update styles for form elements

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import SecPremiumDecoration from "./app/premium.vue"
 import SecButton from "./app/button.vue"
 import SecInput from "./app/input.vue"
 import SecTextarea from "./app/textarea.vue"
+import SecForm from "./app/form.vue"
 
 const switchTheme = ref(true);
 
@@ -234,37 +235,8 @@ function selectSearchResult(val:any) {
 
     <SecTextarea />
 
+    <SecForm />
 
-    <div class="section">
-      <h2 class="section-title">Form</h2>
-      <div class="grid">
-        <div class="form" style="width: 100%">
-          <div class="form-row">
-            <div class="form-row-left">
-              <label class="form-label">General Setting</label>
-              <div class="form-desc">The description about the setting.</div>
-            </div>
-            <div class="form-row-right">
-              <QSwitch v-model="switchValue2" />
-            </div>
-          </div>
-          <div class="form-row column">
-            <div class="form-label">Put a text field</div>
-            <QInput type="text" placeholder="type here!" />
-            <div class="form-hint">
-              What you're typing here will be an example
-            </div>
-          </div>
-          <div class="form-row column">
-            <div class="form-label">Error</div>
-            <div class="form-error">It seems you're typing something wrong</div>
-          </div>
-          <div class="form-row">
-            <QButton class="primary">Save</QButton>
-          </div>
-        </div>
-      </div>
-    </div>
 
     <div class="section">
       <h2 class="section-title">TextField with button</h2>
---
+++ src/app/form.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="section mb-4">
+    <div class="divider mb-4"></div>
+    <h2 class="q-text-h2 mb-4">Form</h2>
+    <div class="grid gap-4 grid-cols-2">
+      <div class="form" style="width: 100%">
+          <div class="form-row">
+            <div class="form-row-left">
+              <label class="form-label">General Setting</label>
+              <div class="form-desc">The description about the setting.</div>
+            </div>
+            <div class="form-row-right">
+              <QSwitch v-model="switchValue" />
+            </div>
+          </div>
+          <div class="form-row column">
+            <div class="form-label">Put a text field</div>
+            <QInput type="text" placeholder="type here!" />
+            <div class="form-hint">
+              What you're typing here will be an example
+            </div>
+          </div>
+          <div class="form-row column">
+            <div class="form-label">Error</div>
+            <div class="form-error">It seems you're typing something wrong</div>
+          </div>
+          <div class="form-row">
+            <QButton class="highlight">Save</QButton>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const switchValue = ref(false);
+</script>
+
+<style scoped lang="scss">
+</style>

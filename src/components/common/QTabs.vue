19847b91c4ffca3255c0375c15c3e18bbf654264
Lyric <5h3ll3x@gmail.com>
2026-01-11 16:12:36 +0900

    Add animated indicator and improved styling for QTabs

--- src/components/common/QTabs.vue
+++ src/components/common/QTabs.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { ref, watch, onMounted, nextTick } from 'vue';
 
 const props = defineProps({
   modelValue: Object,
@@ -20,19 +20,56 @@ const props = defineProps({
 const emit = defineEmits(["change", "update:modelValue"]);
 
 const selectedTab = ref(props.tabs[props.initialIndex]);
-
 const selectedIndex = ref(props.initialIndex);
 
-watch(() => props.modelValue, (tab:any) => {
-	selectedIndex.value = props.tabs.findIndex((t:any) => t.id === tab.id);
+// Refs for indicator animation
+const tabsContainer = ref<HTMLElement | null>(null);
+const tabButtons = ref<HTMLElement[]>([]);
+const indicatorStyle = ref({
+  left: '0px',
+  width: '0px',
+});
+
+function updateIndicator() {
+  nextTick(() => {
+    const container = tabsContainer.value;
+    const button = tabButtons.value[selectedIndex.value];
+    if (container && button) {
+      const containerRect = container.getBoundingClientRect();
+      const buttonRect = button.getBoundingClientRect();
+      indicatorStyle.value = {
+        left: `${buttonRect.left - containerRect.left}px`,
+        width: `${buttonRect.width}px`,
+      };
+    }
+  });
+}
+
+watch(() => props.modelValue, (tab: any) => {
+  selectedIndex.value = props.tabs.findIndex((t: any) => t.id === tab.id);
   selectedTab.value = tab || props.tabs[0];
+  updateIndicator();
+});
+
+watch(selectedIndex, () => {
+  updateIndicator();
+});
+
+onMounted(() => {
+  updateIndicator();
 });
 
 function selectTab(ix: number, tab: any) {
   selectedIndex.value = ix;
   selectedTab.value = tab;
-  emit("change", {index: ix, tab: tab});
+  emit("change", { index: ix, tab: tab });
   emit('update:modelValue', tab);
+}
+
+function setTabButtonRef(el: any, ix: number) {
+  if (el) {
+    tabButtons.value[ix] = el.$el || el;
+  }
 }
 
 
@@ -63,9 +100,12 @@ function genCls(tab:any, ix: number) {
 </script>
 
 <template>
-  <div class="q-tabs" :class="props.variant">
+  <div class="q-tabs" :class="props.variant" ref="tabsContainer">
+    <div class="q-tabs-indicator" :style="indicatorStyle"></div>
     <QButton class="q-tab-button sm" :class="genCls(tab, ix)"
-      v-for="tab, ix in tabs" :key="`tab-button-${ix}`" @click="selectTab(ix, tab)">
+      v-for="tab, ix in tabs" :key="`tab-button-${ix}`"
+      :ref="(el: any) => setTabButtonRef(el, ix)"
+      @click="selectTab(ix, tab)">
       <template v-if="tab.icon">
         <component :is="tab.icon" class="q-tab-button-icon" />
       </template>
@@ -81,7 +121,22 @@ function genCls(tab:any, ix: number) {
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+
+  .q-tabs-indicator {
+    position: absolute;
+    bottom: 0;
+    height: 2px;
+    background-color: var(--q-c-red);
+    // Elastic easing - slight overshoot then settle
+    transition: left 0.4s cubic-bezier(0.34, 1.56, 0.64, 1),
+                width 0.4s cubic-bezier(0.34, 1.56, 0.64, 1);
+    pointer-events: none;
+  }
+
   .q-tab-button {
+    position: relative;
+    z-index: 1;
     &.with-icon.with-label {
       .q-tab-button-icon {
         margin-right: 0.5rem;
@@ -97,16 +152,25 @@ function genCls(tab:any, ix: number) {
     border-radius: 6px;
     background-color: var(--q-bg-light-float);
     padding: .25rem;
+
+    .q-tabs-indicator {
+      top: .25rem;
+      bottom: .25rem;
+      height: auto;
+      background-color: white;
+      border-radius: 4px;
+      border: 0.5px solid var(--q-c-dark-4);
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+    }
+
     &:deep(.q-button) {
       transition: none;
-      &.selected {
-        background-color: white !important;
-      }
+      background-color: transparent !important;
+      border: 0.5px solid transparent !important;
       &.non-selected {
-        border: 0.5px solid transparent !important;
         opacity: 0.6 !important;
         &:hover {
-          background-color: transparent !important;
+          opacity: 0.8 !important;
         }
       }
     }
@@ -114,17 +178,23 @@ function genCls(tab:any, ix: number) {
   &.plain {
     box-shadow: inset 0 -0.5px 0 0 var(--q-c-dark-4);
     padding: 0;
+
+    .q-tabs-indicator {
+      bottom: 0;
+      height: 2px;
+      border-radius: 1px 1px 0 0;
+    }
+
     &:deep(.q-button) {
-      transition: all 0.2s ease-in-out;
+      transition: color 0.2s ease-in-out;
       border-radius: 0 !important;
-      border-bottom: 1px solid transparent;
+      border-bottom: none !important;
       &.squircle {
         --squircle-radius-bottom-left: 0px;
         --squircle-radius-bottom-right: 0px;
       }
       &.selected {
         color: var(--q-c-red) !important;
-        border-bottom: 1px solid var(--q-c-red);
       }
       &.non-selected {
         color: var(--q-c-dark) !important;
@@ -135,24 +205,31 @@ function genCls(tab:any, ix: number) {
     }
   }
 }
+</style>
+
+<style lang="scss">
 .dark {
   .q-tabs {
     &.normal {
       background-color: var(--q-bg-dark-2);
       box-shadow: inset 0 0 0 1px var(--q-c-light-5);
-      &:deep(.q-button) {
-        &.selected {
-          background-color: var(--q-bg-dark-float) !important;
-          border: 0.5px solid var(--q-c-light-4);
-        }
+
+      .q-tabs-indicator {
+        background-color: var(--q-bg-dark-float);
+        border: 0.5px solid var(--q-c-light-4);
+        box-shadow: none;
       }
     }
     &.plain {
       box-shadow: inset 0 -0.5px 0 0 var(--q-c-light-4);
-      &:deep(.q-button) {
+
+      .q-tabs-indicator {
+        background-color: var(--q-c-red-light);
+      }
+
+      .q-button {
         &.selected {
           color: var(--q-c-red-light) !important;
-          border-bottom: 1px solid var(--q-c-red-light);
         }
         &.non-selected {
           color: var(--q-c-light-2) !important;
